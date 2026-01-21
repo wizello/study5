3b59bd2be9022985c42905e2903017bd7acb4bab
ogafro6-art <ogafro6@gmail.com>
2026-01-21 13:28:06 +0000

    Console.WriteLine modified

--- brocodes/Program.cs
+++ brocodes/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("hmmm ");
+Console.WriteLine("i like pizza");
+Console.WriteLine("its tasty");
+int x;
+x = 20;
+
+int y = 10;
+int z = y + x;
+Console.WriteLine("My age is " + y);
+Console.WriteLine("and i like the number " + x);
+Console.WriteLine("that number combined with my age is " + z);
+//or
+Console.WriteLine("I am " + y + " years old");
+
+double height = 100.5;
+Console.WriteLine("My height is " + height + "cm");
+
+bool alive = true;
+Console.WriteLine("are you alive? " + alive);
+
+char symbol = '@';
+Console.WriteLine("your character is " + symbol);
+
+String name = "Ahmed";
+Console.WriteLine("Hello " + name);
+
+String username = symbol + name;
+Console.WriteLine("your username is " + username);
