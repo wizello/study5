0c8f389f2d956353f352da4edf881d58f69805c0
wizello <152424488+wizello@users.noreply.github.com>
2026-01-21 15:31:36 +0000

    Update Program.cs

--- brocodes/Program.cs
+++ brocodes/Program.cs
@@ -11,19 +11,4 @@ Console.WriteLine("My age is " + y);
 Console.WriteLine("and i like the number " + x);
 Console.WriteLine("that number combined with my age is " + z);
 //or
-Console.WriteLine("I am " + y + " years old");
 
-double height = 100.5;
-Console.WriteLine("My height is " + height + "cm");
-
-bool alive = true;
-Console.WriteLine("are you alive? " + alive);
-
-char symbol = '@';
-Console.WriteLine("your character is " + symbol);
-
-String name = "Ahmed";
-Console.WriteLine("Hello " + name);
-
-String username = symbol + name;
-Console.WriteLine("your username is " + username);
